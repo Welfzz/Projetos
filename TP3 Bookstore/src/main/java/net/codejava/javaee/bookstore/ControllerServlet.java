d4fab89229d1fbcdd2c49a8aec6f993a475398bf
Liniker Vaz de Lima <61416550+Welfzz@users.noreply.github.com>
2022-06-19 17:17:00 -0300

    Update ControllerServlet.java

--- TP3 Bookstore/src/main/java/net/codejava/javaee/bookstore/ControllerServlet.java
+++ TP3 Bookstore/src/main/java/net/codejava/javaee/bookstore/ControllerServlet.java
@@ -1,3 +1,7 @@
+/**
+ Dupla: Liniker Vaz de Lima e Diego Negrete Alves
+ */
+
 package net.codejava.javaee.bookstore;
  
 import java.io.IOException;
@@ -9,10 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-/**
- *SWII5 - TP03 - Alunos: Grazielle e Josuel - Prof Tuler
- */
 
 public class ControllerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
